be35fe1ea9a17a0d7cf39748911a91d62daf46a7
Mark Biek <mark.biek@kindred.com>
2015-10-11 16:38:49 -0400

    Math for calculating post offset is working

--- webroot/js/global.js
+++ webroot/js/global.js
@@ -1,11 +1,18 @@
 if(typeof console=='undefined')console={log:function(){}};
 (function($) {
     if($('.posts').length > 0) {
+        var pageSize = parseInt($('.posts').data('pagesize'), 10);
+        var totalPages = Math.ceil(parseInt($('.posts').data('num-posts'), 10) / 10);
+
+        console.log('pageSize=' + pageSize);
+        console.log('totalPages=' + totalPages);
+
         $('#pagination').twbsPagination( {
-            totalPages: parseInt($('.posts').data('num-posts'), 10),
+            totalPages: totalPages,
             visiblePages: 10,
             onPageClick: function(event, page) {
-
+                var offset = ( (page-1) * 10) + 1;
+                console.log('Page ' + page + ', offset ' + offset);
             }
         });
     }
